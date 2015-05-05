f3960cf3025e387c294a942d95a061c28072ea82
Polyxeni Makri <poma@kb.dk>
2015-05-05 14:19:58 +0200

    valhal solr config

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -7,31 +7,166 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
 
   configure_blacklight do |config|
-          config.show.oembed_field = :oembed_url_ssm
-          config.show.partials.insert(1, :oembed)
-          config.view.gallery.partials = [:index_header, :index]
-          config.view.masonry.partials = [:index]
-          config.view.slideshow.partials = [:index]
+                    config.show.oembed_field = :oembed_url_ssm
+                    config.show.partials.insert(1, :oembed)
+                    config.view.gallery.partials = [:index_header, :index]
+                    config.view.masonry.partials = [:index]
+                    config.view.slideshow.partials = [:index]
 
-          config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-          config.show.partials.insert(1, :openseadragon)
+                    config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
+                    config.show.partials.insert(1, :openseadragon)
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
-      qt: 'search',
-      rows: 10,
-      fl: '*'
+        :qf => 'author_tesim title_tesim person_name_tesim',
+        :qt => 'search',
+        #  :fq => "-active_fedora_model_ssi:(Instance OR Trykforlaeg OR ContentFile)", # exclude fileresults and instances from search result
+        :rows => 10
     }
 
-    config.document_solr_path = 'get'
-    config.document_unique_id_param = 'ids'
+    # This filters out objects that you want to exclude from search results, like FileAssets
+    CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
+
+    def exclude_unwanted_models(solr_parameters, user_parameters)
+      solr_parameters[:fq] ||= []
+      unwanted_models.each do |model|
+       #
+       # solr_parameters[:fq] << "-has_model_ssim:\"#{model.to_class_uri}\""
+      end
+    end
+
+    def unwanted_models
+      ['Instance', 'Trykforlaeg', 'ContentFile', 'Administration::Activity']
+    end
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'full_title_tesim'
+    config.index.title_field = 'display_value_ssm'
+    config.index.display_type_field = 'has_model_ssim'
+
+    # solr fields that will be treated as facets by the blacklight application
+    #   The ordering of the field names is the order of the display
+    #
+    # Setting a limit will trigger Blacklight's 'more' facet values link.
+    # * If left unset, then all facet values returned by solr will be displayed.
+    # * If set to an integer, then "f.somefield.facet.limit" will be added to
+    # solr request, with actual solr request being +1 your configured limit --
+    # you configure the number of items you actually want _tsimed_ in a page.
+    # * If set to 'true', then no additional parameters will be sent to solr,
+    # but any 'sniffed' request limit parameters will be used for paging, with
+    # paging at requested limit -1. Can sniff from facet.limit or
+    # f.specific_field.facet.limit solr request params. This 'true' config
+    # can be used if you set limits in :default_solr_params, or as defaults
+    # on the solr side in the request handler itself. Request handler defaults
+    # sniffing requires solr requests to be made with "echoParams=all", for
+    # app code to actually have it echo'd back to see it.
+    #
+    # :show may be set to false if you don't want the facet to be drawn in the
+    # facet bar
+    config.add_facet_field 'author_sim', :label => 'Forfatter'
+    #config.add_facet_field 'active_fedora_model_ssi', :label => 'Indhold', helper_method: :translate_model_names
+    config.add_facet_field 'work_collection_sim', :label => 'Samling'
+    config.add_facet_field 'work_activity_sim', :label => 'Aktivitet', helper_method: :get_activity_name
 
 
-    config.add_sort_field 'relevance', sort: 'score desc', label: 'Relevance'
+    # Have BL send all facet field names to Solr, which has been the default
+    # previously. Simply remove these lines if you'd rather use Solr request
+    # handler defaults, or have no facets.
+    config.default_solr_params[:'facet.field'] = config.facet_fields.keys
+    #use this instead if you don't want to query facets marked :show=>false
+    #config.default_solr_params[:'facet.field'] = config.facet_fields.select{ |k, v| v[:show] != false}.keys
 
-    config.add_field_configuration_to_solr_request!
+
+    # solr fields to be displayed in the index (search results) view
+    #   The ordering of the field names is the order of the display
+    config.add_index_field 'subtitle_tesim', :label => 'Undertitel'
+    config.add_index_field 'author_tesim', :label => 'Forfatter'
+    config.add_index_field 'person_name_tesim', :label => 'Navn'
+
+    # solr fields to be displayed in the show (single result) view
+    #   The ordering of the field names is the order of the display
+    config.add_show_field 'subtitle_tesim', :label => 'Undertitel'
+    config.add_show_field 'author_tesim', :label => 'Forfatter'
+    config.add_show_field 'person_name_tesim', :label => 'Navn'
+
+    # "fielded" search configuration. Used by pulldown among other places.
+    # For supported keys in hash, see rdoc for Blacklight::SearchFields
+    #
+    # Search fields will inherit the :qt solr request handler from
+    # config[:default_solr_parameters], OR can specify a different one
+    # with a :qt key/value. Below examples inherit, except for subject
+    # that specifies the same :qt as default for our own internal
+    # testing purposes.
+    #
+    # The :key is what will be used to identify this BL search field internally,
+    # as well as in URLs -- so changing it after deployment may break bookmarked
+    # urls.  A display label will be automatically calculated from the :key,
+    # or can be specified manually to be different.
+
+    # This one uses all the defaults set by the solr request handler. Which
+    # solr request handler? The one set in config[:default_solr_parameters][:qt],
+    # since we aren't specifying it otherwise.
+
+    config.add_search_field 'all_fields', :label => 'Alle Felter'
+
+    # Now we see how to over-ride Solr request handler defaults, in this
+    # case for a BL "search field", which is really a dismax aggregate
+    # of Solr search fields.
+
+    config.add_search_field('titel') do |field|
+      # :solr_local_parameters will be sent using Solr LocalParams
+      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+      # Solr parameter de-referencing like $title_qf.
+      # See: http://wiki.apache.org/solr/LocalParams
+      field.solr_local_parameters = {
+          :qf => '$title_qf',
+          :pf => '$title_pf'
+      }
+    end
+
+    config.add_search_field('forfatter') do |field|
+      field.solr_local_parameters = {
+          :qf => '$author_qf',
+          :pf => '$author_pf'
+      }
+    end
+
+    # Specifying a :qt only to show it's possible, and so our internal automated
+    # tests can test it. In this case it's the same as
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
+    config.add_search_field('Personer') do |field|
+      field.qt = 'search'
+      field.solr_local_parameters = {
+          :qf => '$subject_qf',
+          :pf => '$subject_pf'
+      }
+    end
+
+    # "sort results by" select (pulldown)
+    # label in pulldown is followed by the name of the SOLR field to sort by and
+    # whether the sort is ascending or descending (it must be asc or desc
+    # except in the relevancy case).
+    config.add_sort_field 'score desc, pub_date_dtsi desc, title_tesi asc', :label => 'relevans'
+    config.add_sort_field 'author_tesi asc, title_tesi asc', :label => 'forfatter'
+    config.add_sort_field 'title_tesi asc, pub_date_dtsi desc', :label => 'title'
+
+    # If there are more than this many search results, no spelling ("did you
+    # mean") suggestion is offered.
+    config.spell_max = 5
+
+
+                    #config.document_solr_path = 'get'
+                    #config.document_unique_id_param = 'ids'
+
+                    # solr field configuration for search results/index views
+                    #config.index.title_field = 'full_title_tesim'
+
+
+                    #config.add_sort_field 'relevance', sort: 'score desc', label: 'Relevance'
+
+                    #config.add_field_configuration_to_solr_request!
+
+
+
   end
 end
+
