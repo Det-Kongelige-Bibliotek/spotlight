82fd3161384530714d32bcf00a82d872478c4382
Polyxeni Makri <poma@kb.dk>
2015-06-10 14:56:56 +0200

    Add facets for the default-exhibit

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,3 +1,4 @@
+
 # -*- encoding : utf-8 -*-
 require 'blacklight/catalog'
 
@@ -7,20 +8,20 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
 
   configure_blacklight do |config|
-          config.show.oembed_field = :oembed_url_ssm
-          config.show.partials.insert(1, :oembed)
-          config.view.gallery.partials = [:index_header, :index]
-          config.view.masonry.partials = [:index]
-          config.view.slideshow.partials = [:index]
+    config.show.oembed_field = :oembed_url_ssm
+    config.show.partials.insert(1, :oembed)
+    config.view.gallery.partials = [:index_header, :index]
+    config.view.masonry.partials = [:index]
+    config.view.slideshow.partials = [:index]
 
-          config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-          config.show.partials.insert(1, :openseadragon)
+    config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
+    config.show.partials.insert(1, :openseadragon)
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
-      qt: 'search',
-      rows: 10,
-      fl: '*'
+        qt: 'search',
+        rows: 10,
+        fl: '*'
     }
 
     config.document_solr_path = 'get'
@@ -29,8 +30,15 @@ class CatalogController < ApplicationController
     # solr field configuration for search results/index views
     config.index.title_field = 'full_title_tesim'
 
-
     config.add_sort_field 'relevance', sort: 'score desc', label: 'Relevance'
+
+    config.add_facet_field 'exhibit_default-exhibit_location_ssim', :label => 'Location'
+    config.add_facet_field 'exhibit_default-exhibit_institution_ssim', :label => 'Institution'
+    config.add_facet_field 'exhibit_default-exhibit_type_ssim', :label => 'Type'
+
+    #config.add_facet_field 'exhibit_mette_type_ssim', :label => 'Type'
+
+    config.default_solr_params[:'facet.field'] = config.facet_fields.keys
 
     config.add_field_configuration_to_solr_request!
   end
